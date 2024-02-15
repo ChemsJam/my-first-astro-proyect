c58f849b6c22e9ed1e0025ae006e8910068caba8
Ontiveros Alfaro Johan Alejandro <johalfaro1174@gmail.com>
2024-02-15 09:07:57 -0600

    FirstCommit

---
+++ src/components/HelloComponent.jsx
@@ -0,0 +1,12 @@
+function HelloComponent() {
+  return (
+    <div>
+        <h1>React Hello</h1>
+        <button onClick={() => {
+            alert("OMG HAZ DADO CLICK :O")
+        }}>Click me pls C:</button>
+    </div>
+  )
+}
+
+export default HelloComponent
